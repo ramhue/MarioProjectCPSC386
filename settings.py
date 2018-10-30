e69e385e21f779333e36faab007f736d745fb17c
axaymountry326 <axaymountry@csu.fullerton.edu>
2018-10-30 15:40:58 -0700

    Settings

---
+++ settings.py
@@ -0,0 +1,15 @@
+# Aaron Xaymountry
+# CPSC 386-01
+# MW 5:30-6:45pm
+# Settings for Super Mario
+
+
+class Settings():
+    def __init__(self):
+        """Initialize the game's static settings."""
+        # Screen settings.
+        self.screen_width = 1024
+        self.screen_height = 576
+        self.scale = 3
+
+
