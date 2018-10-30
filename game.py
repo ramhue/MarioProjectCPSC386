488cc9afd52f0ee557ec0818932abb6bc91e049a
margisj <37324267+margisj@users.noreply.github.com>
2018-10-30 14:33:11 -0700

    Update game.py

---
+++ game.py
@@ -0,0 +1,25 @@
+import pygame
+from spritesheet import SpriteSheet
+
+class Game():
+
+    def __init__(self):
+
+        self.scale = 3
+
+        pygame.init()
+        self.background = pygame.image.load('background.png')
+        self.background = pygame.transform.scale(self.background, (self.background.get_width()*self.scale,
+                                                                   self.background.get_height()*self.scale))
+        self.backrect = self.background.get_rect()
+        self.backrect.x -= 400*self.scale
+        self.screen = pygame.display.set_mode((256*self.scale, 224*self.scale))
+
+    def play(self):
+        while True:
+            self.screen.blit(self.background, self.backrect)
+            pygame.display.flip()
+
+
+game = Game()
+game.play()
