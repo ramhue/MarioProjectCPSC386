c90e4fb92f68c3ed601029600acb52a18f468a36
axaymountry326 <axaymountry@csu.fullerton.edu>
2018-10-30 16:46:23 -0700

    Displays scoring info at top screen

--- gameStats.py
+++ gameStats.py
@@ -14,37 +14,25 @@ class GameStats():
 
         # Score display text
         self.text_color = (30, 30, 30)
-        font = pygame.font.Font(None, 36)
-        self.font = pygame.font.SysFont(None, 36)
-        self.Scores = font.render("SCORE", 2, WHITE)
+        self.font = pygame.font.Font('images/ARCADECLASSIC.TTF', 36)
+        self.Scores = self.font.render("SCORE", 2, WHITE)
         self.ScoresPos = self.Scores.get_rect()
         self.ScoresPos = (((self.settings.screen_width / 2) - 475), (self.settings.screen_height / 2) - 275)
 
         # Coins display text
-        self.text_color = (30, 30, 30)
-        font = pygame.font.Font(None, 36)
-        self.font = pygame.font.SysFont(None, 36)
-        self.Coins = font.render("COINS", 2, WHITE)
+        self.Coins = self.font.render("COINS", 2, WHITE)
         self.CoinsPos = self.Coins.get_rect()
         self.CoinsPos = ((self.settings.screen_width / 2) - 275, (self.settings.screen_height / 2) - 275)
 
         # Time display text
-        self.text_color = (30, 30, 30)
-        font = pygame.font.Font(None, 36)
-        self.font = pygame.font.SysFont(None, 36)
-        self.Time = font.render("TIME", 2, WHITE)
+        self.Time = self.font.render("TIME", 2, WHITE)
         self.TimePos = self.Time.get_rect()
-        self.TimePos = ((self.settings.screen_width / 2) - 50, (self.settings.screen_height / 2) - 275)
+        self.TimePos = ((self.settings.screen_width / 2) - 75, (self.settings.screen_height / 2) - 275)
 
         # Lives display text
-        self.text_color = (30, 30, 30)
-        font = pygame.font.Font(None, 36)
-        self.font = pygame.font.SysFont(None, 36)
-        self.Lives = font.render("LIVES", 2, WHITE)
+        self.Lives = self.font.render("LIVES", 2, WHITE)
         self.LivesPos = self.Lives.get_rect()
-        self.LivesPos = ((self.settings.screen_width / 2) + 150, (self.settings.screen_height / 2) - 275)
-
-
+        self.LivesPos = ((self.settings.screen_width / 2) + 125, (self.settings.screen_height / 2) - 275)
 
     def blitstats(self):
         self.screen.blit(self.Scores, self.ScoresPos)
