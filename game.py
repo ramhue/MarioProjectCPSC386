f19ac003fbcf0daab2a8d2daa29e2e4859c50efe
margisj <37324267+margisj@users.noreply.github.com>
2018-10-30 14:39:14 -0700

    Add side scrolling code

--- game.py
+++ game.py
@@ -12,11 +12,17 @@ class Game():
         self.background = pygame.transform.scale(self.background, (self.background.get_width()*self.scale,
                                                                    self.background.get_height()*self.scale))
         self.backrect = self.background.get_rect()
-        self.backrect.x -= 400*self.scale
+        # self.backrect.x -= 400*self.scale
         self.screen = pygame.display.set_mode((256*self.scale, 224*self.scale))
 
     def play(self):
         while True:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.backrect.x -= 10*self.scale
+                    elif event.key  == pygame.K_LEFT:
+                        self.backrect.x += 10*self.scale
             self.screen.blit(self.background, self.backrect)
             pygame.display.flip()
 
