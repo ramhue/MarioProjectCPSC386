199afa5e0ed55c55a2970c35e75c21c905c7c9c7
margisj <37324267+margisj@users.noreply.github.com>
2018-10-30 15:15:26 -0700

    Changed sidescroll to camera

--- game.py
+++ game.py
@@ -11,19 +11,20 @@ class Game():
         self.background = pygame.image.load('background.png')
         self.background = pygame.transform.scale(self.background, (self.background.get_width()*self.scale,
                                                                    self.background.get_height()*self.scale))
-        self.backrect = self.background.get_rect()
-        # self.backrect.x -= 400*self.scale
         self.screen = pygame.display.set_mode((256*self.scale, 224*self.scale))
+
+        self.camera = self.screen.get_rect()
+        self.camera.x = 0
 
     def play(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT:
-                        self.backrect.x -= 10*self.scale
+                        self.camera.x += 10*self.scale
                     elif event.key == pygame.K_LEFT:
-                        self.backrect.x += 10*self.scale
-            self.screen.blit(self.background, self.backrect)
+                        self.camera.x -= 10*self.scale
+            self.screen.blit(self.background, (0, 0), self.camera)
             pygame.display.flip()
 
 
