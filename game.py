7a7629288d5b399d9584f13a00940e042fd5ae42
margisj <37324267+margisj@users.noreply.github.com>
2018-10-30 17:06:27 -0700

    Added camera screen movement

--- game.py
+++ game.py
@@ -12,25 +12,39 @@ WHITE = (250, 250, 250)
 BLUE = (132, 112, 255)
 
 
-def Game():
-    pygame.init()
-    gamesettings = Settings()
-    scale = 3
-    screen = pygame.display.set_mode((256 * scale, 224 * scale))
-    pygame.display.set_caption("Super Mario")
-    background = Stage_Background(screen)
-    player = Mario(screen, gamesettings)
-    thegoomba = Goomba(screen, gamesettings)
-    screen.fill(BLACK)
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.scale = 3
+        self.screen = pygame.display.set_mode((256 * self.scale, 224 * self.scale))
+        self.gamesettings = Settings(self.screen)
+        pygame.display.set_caption("Super Mario")
+        # self.background = Stage_Background(self.screen)
+        self.background = pygame.image.load('images/marioW1.png')
+        self.background = pygame.transform.scale(self.background, (self.background.get_width() * self.scale,
+                                                                   self.background.get_height() * self.scale))
+        self.rect = self.background.get_rect()
+        self.level = pygame.Surface((self.rect.width, self.rect.height))
+        self.player = Mario(self.level, self.gamesettings)
+        self.thegoomba = Goomba(self.level, self.gamesettings)
+        self.screen.fill(BLACK)
 
-    while True:
-        screen.fill(BLACK)
-        # CHANGE TO MARIO
-        gf.check_events(player, thegoomba, background)
-        background.blitbackground()
-        thegoomba.blitGoomba()
-        player.blitMario()
+    def play(self):
+        while True:
+            self.screen.fill(BLACK)
+            # CHANGE TO MARIO
+            gf.check_events(self.player, self.thegoomba, self.background)
+            # self.background.blitbackground()
+            self.blit()
+
+    # Blit everything to self.level then blit self.level
+    def blit(self):
+        self.level.blit(self.background, self.gamesettings.camera, self.gamesettings.camera)
+        self.thegoomba.blitGoomba()
+        self.player.update()
+        self.screen.blit(self.level, (0, 0), self.gamesettings.camera)
         pygame.display.flip()
+
 
 game = Game()
 game.play()
--- settings.py
+++ settings.py
@@ -2,11 +2,14 @@
 
 
 class Settings():
-    def __init__(self):
+    def __init__(self, screen):
         """Initialize the game's static settings."""
         # Screen settings.
-        self.screen_width = 1024
-        self.screen_height = 576
+        self.screen = screen
+        self.screen_width = self.screen.get_width()
+        self.screen_height = self.screen.get_height()
         self.scale = 3
+        self.camera = self.screen.get_rect()
+        self.camera.x = 0
 
 
