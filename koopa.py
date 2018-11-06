e34ffdfceba67bea11335f790b1b214c2757a142
axaymountry326 <axaymountry@csu.fullerton.edu>
2018-11-05 17:34:59 -0800

    work in progress

--- koopa.py
+++ koopa.py
@@ -1,32 +1,32 @@
-# Goomba enemy class
+# Koopa enemy class
 
 import pygame
-from settings import Settings
 from spritesheet import SpriteSheet
 
-class Goomba():
+class Koopa():
     def __init__(self, screen, settings):
         self.screen = screen
         self.settings = settings
         self.sprite = SpriteSheet('images/spritesheet.png')
-        self.sprite.get_image(0, 0, 10, 10)
+        self.sprite.get_image(144, 0, 30, 26)
         self.images = list()
         self.width = 50
         self.height = 50
-        self.images.append(self.sprite.get_image(0, 0, 16, 20))
-        self.images.append(self.sprite.get_image(30, 0, 16, 20))
-        self.images.append(self.sprite.get_image(60, 0, 16, 20))
-        for i in range(3):
+        self.images.append(self.sprite.get_image(144, 0, 30, 26))
+        self.images.append(self.sprite.get_image(174, 0, 30, 26))
+        self.images.append(self.sprite.get_image(204, 0, 30, 26))
+        self.images.append(self.sprite.get_image(234, 0 , 30, 26))
+        for i in range(4):
             self.images[i] = pygame.transform.scale(self.images[i], (self.width, self.height))
         self.rect = self.images[0].get_rect()
-        self.rect.x, self.rect. y = 50 * self.settings.scale, 50 * self.settings.scale
 
-    def blitGoomba(self):
+    def blitKoopa(self):
         if pygame.time.get_ticks() % 200 <= 50:
             self.screen.blit(self.images[0], self.rect)
         #elif pygame.time.get_ticks() % 200 <= 100:
         else:
             self.screen.blit(self.images[1], self.rect)
+        # Need to flip koopa if walking on otherside
         '''
         elif pygame.time.get_ticks() % 200 <= 150:
             self.screen.blit(self.images[0], self.rect)
